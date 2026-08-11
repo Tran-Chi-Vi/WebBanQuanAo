a7b05e561dfd465761feca7999e6aea5414c59c1
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-11 21:57:08 +0700

    Fix giao dien he thong

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3e660f45de4f7212414da8c6869088f09489ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d2249fc4313884aa3c43f14d112fe251bdab912")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
