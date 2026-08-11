cadaf51052650299a5067830fd38dfa791eddf18
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-11 22:58:04 +0700

    Update single LED beam animation, home banner particles, and Google/Facebook avatar claims

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2740b835acc8fe1621340932deaff575b87e210")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3fa6515ca734f0128acb8975ba78dfd8ed48313")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
