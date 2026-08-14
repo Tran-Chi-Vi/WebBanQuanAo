11173c0884d34b20f1cf79d2792389bedbc3ffd9
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-15 03:20:21 +0700

    Expand single motion beam to 130px width and 10px height with luxury laser gradient core

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e3dc7c9b2504fa8a1f995660cec31e12ece3119")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed6242f4b69d1ad23b19ebdabf2d1aa86a88b64")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
