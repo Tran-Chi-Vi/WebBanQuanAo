bf1476abc15028e94a3ccd84ee1413587602df42
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-15 01:35:23 +0700

    Fix SVG stroke swelling on vertical edges with non-scaling-stroke and set 50px line length

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+397461606d6177f586fb51fc8035aeeccf08a667")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d56f7653309d4f8963f28a27a774073d6d256ed0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
