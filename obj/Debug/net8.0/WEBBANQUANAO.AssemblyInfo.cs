989a9ebcfaf41f4f8fcc22d43265d968fbe83f0e
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-17 02:30:02 +0700

    Fix DOM hoisting bug by moving modal markup outside table elements so all products open clean popups

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b40398837f757e3257b4b336f073f79c3a4ec046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10dda41e22b71f114a781840ca2a1595116f849f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
