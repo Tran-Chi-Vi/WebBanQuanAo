902f261b5882eb37c679a49937f3efff3ff16740
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-19 21:30:55 +0700

    Polish user dark mode dropdown menu contrast and hover states in site.css (1/10)

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55dd5dd530be73fa4138aa66cad06f5200acd07d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364703e4f0def88430b4715177f3f607adb97bd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
