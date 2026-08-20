7893034a0d586907c1bf3745e3e69d87df382ae4
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-20 23:21:13 +0700

    Use theme-adaptive bg-body class for saved address card container in Profile

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+562d7bc9d8377ae2928a9b7914147efcbd1ae585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdfaa8ecaa393a64a6d0f70d23bb67f1bd42187e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
