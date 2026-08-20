edbe7000bef5b8d1e8808d66331341ee6eda9910
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-21 01:27:44 +0700

    Remove emoji from logistics header badge in Track

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d67a2d881d29c3fd1dafd0bd23f11df56175d18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+609d3bd51ebe01e0809fb9ddc34fbeec217e7dd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
