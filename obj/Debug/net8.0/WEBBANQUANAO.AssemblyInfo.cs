9d4b32f507ddee63d727c38c17aa9518476499f7
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-21 01:28:45 +0700

    Use subtle rounded-pill status badges in Admin ProductManagement Index

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edbe7000bef5b8d1e8808d66331341ee6eda9910")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a4fb5853316350ca36c7f46feae3e048ac1af5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
