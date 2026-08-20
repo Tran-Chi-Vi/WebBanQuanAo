d0d68e3933c2c8e80493e85aeb0204e7365e6ca6
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-20 23:24:52 +0700

    Use theme-adaptive bg-body and text-body classes in Admin OrderManagement filter card

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c70ac53c068c84935653041bf9be1bb7251d149")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8558cc8f6eb60ab64a2d8a77c951f43f684cdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
