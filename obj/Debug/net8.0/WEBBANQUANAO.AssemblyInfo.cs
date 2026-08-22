8dbb9bdf6b2de9e88429e5917cf6f7abab98d94e
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-22 22:03:31 +0700

    Polish empty cart icon color opacity and title in Cart Index

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8177645ee3dae452db4b67fd8a2f67fe775ce07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f973b6326136e487f34b5c5a7403b56f890b5876")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
