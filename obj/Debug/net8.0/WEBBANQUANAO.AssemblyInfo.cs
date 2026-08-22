125234c53c0a137cb9e34866258375d393707bbf
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-23 01:03:36 +0700

    Add aria-label and title tooltip to promo code input in Cart Index

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e028a8109b18dc543519cd304ea84485a37a8879")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+284435458dabd1876af00b434f64b04a824867eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
