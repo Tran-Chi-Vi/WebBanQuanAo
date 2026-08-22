a9cbd54ad05e46badc6382c000bac57344435587
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-23 00:33:20 +0700

    Use theme-adaptive text-body class in Home Index testimonials

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21c2f4cb62b62fcd1f3802dc51edf09944740465")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85976c2186ab535403291d6f663dbfb6987e9e00")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
