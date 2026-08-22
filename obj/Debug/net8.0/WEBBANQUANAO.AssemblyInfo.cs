e8177645ee3dae452db4b67fd8a2f67fe775ce07
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-22 21:47:14 +0700

    Use theme-adaptive text-body class in Home Index category title and showcase header

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71fd3b459efeee3f891855637b399362340e0b66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746d3d8b6b80f37e77e4da7c94e0cbcec5c15749")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
