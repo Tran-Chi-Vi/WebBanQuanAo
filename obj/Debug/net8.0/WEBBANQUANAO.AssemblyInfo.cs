5716a272cee99b0765744633dd103e948affedcc
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-24 01:20:43 +0700

    Add title tooltips to KPI 3 and 4 titles in Analytics Index

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20a50955d95ecc7c216f2af855bc446b6e956830")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+807117287bddf36717f01c374d441883dc80e970")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
