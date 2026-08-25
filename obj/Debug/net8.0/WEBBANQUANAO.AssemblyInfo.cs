34c428d43409763bed56060d7bcb335a710f238f
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-25 22:52:58 +0700

    Add aria-label to category select dropdown in Product Index

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a1d64204da71e6b6e32cc4cd9821cb263c8f9fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70cca2379d1e4cdd471fe80257a045ce9808facb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
