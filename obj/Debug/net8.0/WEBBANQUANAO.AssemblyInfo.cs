6fc9c85890c0b1e8f139adcf7fbf6fc66d38fbed
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-27 22:32:52 +0700

    Use theme-adaptive text-body and add title tooltip to Select All checkbox in Cart Index

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3803bf89bc81fdc9bce8aa2d4cbc3bb639bbc8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23daf7822884b38a46bfdccf38be609d9d6f5af5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
