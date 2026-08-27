23daf7822884b38a46bfdccf38be609d9d6f5af5
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-27 22:32:23 +0700

    Add title tooltip and aria-label to write review button in Details

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f139686315cabaef76b150139bb975ff1e2a9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3803bf89bc81fdc9bce8aa2d4cbc3bb639bbc8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
