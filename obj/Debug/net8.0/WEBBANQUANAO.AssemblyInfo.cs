b8abe6b25c10743b45c0dee0c2bd90b01cbe6f41
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-31 00:28:28 +0700

    Use theme-adaptive text-body and add aria-label to apply promo button in Cart Index

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a2efdfd156ded9158ecf70e27a75ed963380c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c8983ada23deb0fa799bc6934e2afd39ac7d681")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
