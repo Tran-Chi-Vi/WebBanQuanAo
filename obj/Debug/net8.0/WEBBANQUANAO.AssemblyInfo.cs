9c8983ada23deb0fa799bc6934e2afd39ac7d681
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-08-31 00:27:55 +0700

    Add title tooltip and aria-label to purchaseQuantity input in Details

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b4b08275518bbf98680d01d1203ee82b81b9c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a2efdfd156ded9158ecf70e27a75ed963380c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
