fae91ea775d7b717d7c921ad781a1d1497aef091
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-09-01 16:52:54 +0700

    Add title tooltip to search input in OrderManagement Index

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c111ceb0563f038a36710b2958e06758fdc6813")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf1fa73bf7fb481da2355f76710e93dfee4004eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
