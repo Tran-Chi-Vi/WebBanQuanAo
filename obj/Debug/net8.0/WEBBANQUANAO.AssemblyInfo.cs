d8f0bbe226eeacee259a4778f71b3077c2420bbc
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-09-01 16:48:24 +0700

    Add title tooltips and aria-labels to Category and Brand filters in Product Index

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4732c333c6ba50bf2fe5c021ecf8674676759bed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12fe272a6648ddfddf88b577ac669c643fb8af8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
