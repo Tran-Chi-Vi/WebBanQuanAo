e588477325e2ce4cfe75fb2c5ae16af072a6656f
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-09-03 00:07:31 +0700

    Add title tooltip and aria-label to Register submit button in Register

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c793e02ada70d14608713087d25f06c108dcc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d488a84c90a0b7b09d1638843b036746b138c7ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
