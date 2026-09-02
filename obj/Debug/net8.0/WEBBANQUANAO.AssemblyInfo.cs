a1c793e02ada70d14608713087d25f06c108dcc3
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-09-03 00:05:37 +0700

    Add title tooltips to Profile and MyOrders dropdown links in _Layout

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73eab4746e3c28705485a49da4ad6c0043e702b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89c0a99d265dd211b3ad6d786289dd5520d19e9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
