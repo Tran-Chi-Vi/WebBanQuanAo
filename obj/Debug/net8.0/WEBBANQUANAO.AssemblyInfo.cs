a31de322db7e14f7ef759d1b6320c347c1465782
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-09-04 23:02:21 +0700

    Add title tooltip to Home breadcrumb link in Details

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e50ca1e9aee18d66006af0ad8076fc386a137ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1053b8f7efcf0d5067e85b615ec3eeb71b47e56")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
