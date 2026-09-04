75b25c261a4c5ff70676225235342830d169cc7c
Tran-Chi-Vi <tranchivi29102005@gmail.com>
2026-09-04 23:06:56 +0700

    Add title tooltip to Add Category button in Categories

--- obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
+++ obj/Debug/net8.0/WEBBANQUANAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a31de322db7e14f7ef759d1b6320c347c1465782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50df3e0dcfa9b8d470fec82d37f66504f9e1668f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEBBANQUANAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
